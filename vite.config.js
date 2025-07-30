b2204e3e8e5d68c1a45646c27065385f0b5bf50a
Utkarsh Turkane <utkarshturkane@gmail.com>
2025-07-30 21:21:07 +0530

    vite-cofing

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/utkarsh-turkane-multi-theme-app', 
+  base: '/utkarsh-turkane-multi-theme-app/',
 })
